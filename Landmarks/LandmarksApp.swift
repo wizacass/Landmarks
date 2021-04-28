3641344ff1d968860558a7dbcc1e9521f728e619
Wizacass <visak.pet0@gmail.com>
2021-04-28 23:05:50 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vytenis Petrauskas on 2021-04-28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
